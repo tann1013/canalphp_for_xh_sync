6c3398bcb774e2ead79d3a19f5cbe8de4b61089b
谭健 <tann1013@hotmail.com>
2021-07-22 17:46:52 +0800

    feat#canal支持rabbitmq

--- fuseRabbitmq/publisher.php
+++ fuseRabbitmq/publisher.php
@@ -15,21 +15,31 @@ $config = array(
     'login' => 'guest',
     'password' => 'guest'
 );
+/**
+ * 一、初始化
+ */
 $cnn = new AMQPConnection($config);
 if (!$cnn->connect()) {
-    echo "Cannot connect to the broker";
-    exit();
+    echo "Cannot connect to the broker";exit();
 }
 $ch = new AMQPChannel($cnn);
 $ex = new AMQPExchange($ch);
-//消息的路由键，一定要和消费者端一致
-$routingKey = 'key_1';
-//交换机名称，一定要和消费者端一致，
-$exchangeName = 'exchange_1';
+
+/**
+ * 二、设置消息路由键和交换机名称
+ */
+$routingKey = 'key_1';//消息的路由键，一定要和消费者端一致
+$exchangeName = 'exchange_1';//交换机名称，一定要和消费者端一致，
 $ex->setName($exchangeName);
+/**
+ * 三、其他设置
+ */
 $ex->setType(AMQP_EX_TYPE_DIRECT);
 $ex->setFlags(AMQP_DURABLE);
 $ex->declareExchange();
+/**
+ * 四、发送消息
+ */
 //创建10个消息
 for ($i=1;$i<=999;$i++){
     //消息内容
@@ -38,7 +48,11 @@ for ($i=1;$i<=999;$i++){
         'hello' => 'world',
     );
     //发送消息到交换机，并返回发送结果
-    //delivery_mode:2声明消息持久，持久的队列+持久的消息在RabbitMQ重启后才不会丢失
-    echo "Send Message:".$ex->publish(json_encode($msg), $routingKey, AMQP_NOPARAM, array('delivery_mode' => 2))."\n";
+    echo "Send Message:".$ex->publish(
+        json_encode($msg),
+        $routingKey,
+        AMQP_NOPARAM,
+        array('delivery_mode' => 2)//delivery_mode:2声明消息持久，持久的队列+持久的消息在RabbitMQ重启后才不会丢失
+        )."\n";
     //代码执行完毕后进程会自动退出
 }
